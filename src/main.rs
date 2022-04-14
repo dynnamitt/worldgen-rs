e3878f5b139b41e6dd32a3e3cb6cf1ac0e20d12b
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-04-14 11:02:05 +0200

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -24,22 +24,24 @@ impl Iterator for PsudoRng {
 
 // Infinite "line of numbers"
 // going from center (west[0]) out into 2 directions"
-struct Longitude {
+pub struct Longitude {
     east: PsudoRng,
     west: PsudoRng,
 }
 
 impl Longitude {
-    fn new(e_seed: u64, w_seed: u64) -> Self {
+    pub fn new(e_seed: u64, w_seed: u64) -> Self {
         let eg = PsudoRng::new(e_seed);
         let wg = PsudoRng::new(w_seed);
         Self { east: eg, west: wg }
     }
 
-    fn take_finite(self, x: i64, len: usize) -> Vec<u64> {
+    pub fn take_finite(self, x: i64, len: usize) -> Vec<u64> {
         if x >= 0 {
+            // westward
             self.west.into_iter().skip(x as usize).take(len).collect()
         } else if (x.abs() as usize) >= len {
+            // eastward .rev[erse]
             self.east
                 .skip(x.abs() as usize - 1)
                 .take(len)
@@ -48,37 +50,66 @@ impl Longitude {
                 .rev()
                 .collect()
         } else {
+            // bordering
             todo!()
         }
     }
 }
 
-struct WorldGrid(u64, u64);
+pub type Viewport = (i64, i64, usize, usize);
 
-//
-//  Take out a finite vec of Longitudes
-//  Select an Y-pos where
-//     0  == first southward long.
-//     -1 == first northward long.
+#[derive(PartialEq, Eq)]
+pub enum Dir {
+    Normal,
+    Reverse,
+}
+pub struct WorldGrid(u64, u64);
+
 impl WorldGrid {
-    fn longitudes(self, y: i64, h: usize, x: i64, len: usize) -> Vec<Vec<u64>> {
+    //  Take out a finite vec of Longitudes
+    //  Select an Y-pos where
+    //     0  == first southward long.
+    //     -1 == first northward long.
+    pub fn longitudes(self, vp @ (x, y, w, h): Viewport) -> Vec<Vec<u64>> {
+        let n_seed = self.0;
+        let s_seed = self.1;
         if y >= 0 {
-            let (south_east_xs, south_west_xs): (Vec<_>, Vec<_>) = PsudoRng::new(self.1)
-                .skip(y as usize * 2)
-                .take(h * 2)
-                .enumerate()
-                .partition(|(i, _)| i % 2 == 0);
-
-            south_east_xs
-                .iter()
-                .zip(south_west_xs)
-                .map(|((_, east_s), (_, west_s))| {
-                    Longitude::new(*east_s, west_s).take_finite(x, len)
-                })
-                //.flatten()
-                .collect()
+            // southward
+            WorldGrid::one_dir(s_seed, Dir::Normal, vp)
+        } else if (y.abs() as usize) >= h {
+            // northward .rev[erse]
+            WorldGrid::one_dir(n_seed, Dir::Reverse, vp)
         } else {
-            todo!()
+            // bordering
+            let vp_s = (x, y, w, h);
+            let vp_n = (x, y, w, h);
+            let south_xs = WorldGrid::one_dir(s_seed, Dir::Normal, vp_s);
+            let mut north_xs = WorldGrid::one_dir(n_seed, Dir::Reverse, vp_n);
+            north_xs.extend(south_xs);
+            north_xs
+        }
+    }
+
+    fn one_dir(master_seed: u64, dir: Dir, (x, y, w, h): Viewport) -> Vec<Vec<u64>> {
+        let skips = y.abs() as usize;
+        let skips = if dir == Dir::Normal { skips - 1 } else { skips };
+
+        let (e_seeds, w_seeds): (Vec<_>, Vec<_>) = PsudoRng::new(master_seed)
+            .skip(skips * 2)
+            .take(h * 2)
+            .enumerate()
+            .partition(|(i, _v)| i % 2 == 0);
+
+        let seed_pairs = e_seeds.iter().zip(w_seeds);
+
+        let xs: Vec<Vec<u64>> = seed_pairs
+            .map(|((_i, e_seed), (_j, w_seed))| Longitude::new(*e_seed, w_seed).take_finite(x, w))
+            .collect();
+
+        if dir == Dir::Normal {
+            xs
+        } else {
+            xs.into_iter().rev().collect()
         }
     }
 }
@@ -94,8 +125,8 @@ fn main() {
     let g = PsudoRng::new(2121);
 
     let w = WorldGrid(2021, 2022);
-    let h: usize = 7;
-    let grid = w.longitudes(0, h, 0, 3);
+
+    let grid = w.longitudes((0, -10, 4, 4));
     for g in grid.iter().enumerate() {
         println!(" {:?}", g);
     }
