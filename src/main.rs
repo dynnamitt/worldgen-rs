502c50036183c3cd15d9e3d5b8f4514cbd076d7b
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-04-21 14:17:55 +0200

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,61 @@
 mod world;
 use std::time::*;
+use termion::{clear, color, cursor, style, terminal_size};
 use world::*;
 
+#[allow(dead_code)]
 fn fraction(i: u64) -> f64 {
     let max = u64::MAX as f64;
     i as f64 % max / max
 }
 
+fn colorize_row(it: impl IntoIterator<Item = u64>) -> String {
+    it.into_iter()
+        .map(fraction)
+        .map(|fr| color_utils::AnsiColors::sample(fr, color_utils::GRAYSCALE))
+        .map(color::AnsiValue::grayscale)
+        .map(|a| a.bg_string() + " ")
+        .collect::<Vec<String>>()
+        .join("")
+}
+
 fn main() {
+    println!("{}{}{}", clear::All, style::Reset, cursor::Goto(1, 1));
+    let seeds = (329329892390, 32309302);
+    let (cols, rows) = terminal_size().unwrap(); // never use it :-p
+
+    for i in [-2, -1, 0, 1, 2, 3, 4, 5] {
+        let vp = (-i, -i, cols as usize, rows as usize);
+        let grid1 = world(seeds, vp);
+
+        grid1
+            .into_iter()
+            .map(colorize_row)
+            .for_each(|r| print!("{}", r));
+
+        println!("{}", cursor::Goto(1, 1));
+    }
+}
+
+#[allow(dead_code)]
+mod color_utils {
+    pub type ColorBounds = (u8, u8);
+    pub const RGB: ColorBounds = (16, 232);
+    pub const GRAYSCALE: ColorBounds = (232, 255);
+    pub const BRIGHT: ColorBounds = (8, 16);
+    pub const STD_LOW: ColorBounds = (0, 8);
+
+    pub struct AnsiColors {}
+    impl AnsiColors {
+        pub fn sample(frac: f64, bs: ColorBounds) -> u8 {
+            let len = bs.1 - bs.0;
+            (len as f64 * frac) as u8
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn tests() {
     let now = Instant::now();
 
     let seeds = (33, 77777);
@@ -22,7 +70,7 @@ fn main() {
     println!("-- NOW BORDERING");
     for (x, y) in [(0, -3), (-3, 0), (-3, -3)] {
         println!("Start pos {},{}", x, y);
-        let grid1 = world(seeds, (x, y, 4, 4));
+        let grid1 = world(seeds, (x, y, 45, 45));
         for row in grid1.iter().enumerate() {
             println!(" {:?}", row);
         }
--- src/world.rs
+++ src/world.rs
@@ -1,35 +1,37 @@
 use rand::prelude::*;
 use rand_chacha::ChaCha8Rng;
 
+#[allow(dead_code)]
+
 pub type Viewport = (i64, i64, usize, usize);
 pub type Seeds = (u64, u64);
 
 pub fn world((ns, ss): Seeds, vp @ (x, y, w, h): Viewport) -> Vec<Vec<u64>> {
     if y >= 0 {
         // southward
-        hemisphere(ss, Dir::Normal, vp)
+        hemisphere(ss, Direction::Normal, vp)
     } else if (y.abs() as usize) >= h {
         // northward .rev[erse]
-        hemisphere(ns, Dir::Reverse, vp)
+        hemisphere(ns, Direction::Reverse, vp)
     } else {
         // bordering
         let vp_s: Viewport = (x, 0, w, (h as i64 + y) as usize);
         let vp_n: Viewport = (x, y, w, y.abs() as usize);
-        let south_xs = hemisphere(ss, Dir::Normal, vp_s);
-        let mut north_xs = hemisphere(ns, Dir::Reverse, vp_n);
+        let south_xs = hemisphere(ss, Direction::Normal, vp_s);
+        let mut north_xs = hemisphere(ns, Direction::Reverse, vp_n);
         north_xs.extend(south_xs);
         north_xs
     }
 }
 
 #[derive(PartialEq, Eq, Debug)]
-pub enum Dir {
+pub enum Direction {
     Normal,
     Reverse,
 }
-impl Dir {
+impl Direction {
     fn organize<T>(self, xs: Vec<T>) -> Vec<T> {
-        if self == Dir::Reverse {
+        if self == Direction::Reverse {
             xs.into_iter().rev().collect()
         } else {
             xs
@@ -37,7 +39,7 @@ impl Dir {
     }
 }
 
-fn hemisphere(master_seed: u64, dir: Dir, (x, y, w, h): Viewport) -> Vec<Vec<u64>> {
+fn hemisphere(master_seed: u64, dir: Direction, (x, y, w, h): Viewport) -> Vec<Vec<u64>> {
     let skips = y.abs() as usize;
 
     let (e_seeds, w_seeds): (Vec<_>, Vec<_>) = PsudoRng::new(master_seed)
@@ -65,21 +67,26 @@ impl Longitudes {
         //println!("Skips = {}", skips);
         if x >= 0 {
             // westward
-            Longitudes::take_from_one_dir(Dir::Normal, w_seed, skips, w)
+            Longitudes::take_from_one_dir(Direction::Normal, w_seed, skips, w)
         } else if (x.abs() as usize) >= w {
             // eastward .rev[erse]
-            Longitudes::take_from_one_dir(Dir::Reverse, e_seed, skips, w)
+            Longitudes::take_from_one_dir(Direction::Reverse, e_seed, skips, w)
         } else {
             // bordering
-            let west_xs =
-                Longitudes::take_from_one_dir(Dir::Normal, w_seed, 0, (w as i64 + x) as usize);
-            let mut east_xs = Longitudes::take_from_one_dir(Dir::Reverse, e_seed, skips, skips);
+            let west_xs = Longitudes::take_from_one_dir(
+                Direction::Normal,
+                w_seed,
+                0,
+                (w as i64 + x) as usize,
+            );
+            let mut east_xs =
+                Longitudes::take_from_one_dir(Direction::Reverse, e_seed, skips, skips);
             east_xs.extend(west_xs);
             east_xs
         }
     }
 
-    fn take_from_one_dir(dir: Dir, seed: u64, skips: usize, w: usize) -> Vec<u64> {
+    fn take_from_one_dir(dir: Direction, seed: u64, skips: usize, w: usize) -> Vec<u64> {
         let rng = PsudoRng::new(seed);
         let xs: Vec<u64> = rng.skip(skips).take(w).collect();
         dir.organize(xs)
